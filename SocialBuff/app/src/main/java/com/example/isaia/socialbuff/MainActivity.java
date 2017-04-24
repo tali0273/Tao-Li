3da218611fc9da801bc1b4dd584a5c14dd6901be
Isaiah <issi7915@colorado.edu>
2017-04-23 23:30:51 -0600

    News Feed Styled

--- SocialBuff/app/src/main/java/com/example/isaia/socialbuff/MainActivity.java
+++ SocialBuff/app/src/main/java/com/example/isaia/socialbuff/MainActivity.java
@@ -17,6 +17,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -48,7 +49,7 @@ public class MainActivity extends AppCompatActivity
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
-
+        
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
