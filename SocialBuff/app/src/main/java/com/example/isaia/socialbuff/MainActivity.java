0f97b76e88fe543d2c01120fe00ddb2b4939b4b2
Isaiah <issi7915@colorado.edu>
2017-04-25 12:13:56 -0600

    Profile Page Test

--- SocialBuff/app/src/main/java/com/example/isaia/socialbuff/MainActivity.java
+++ SocialBuff/app/src/main/java/com/example/isaia/socialbuff/MainActivity.java
@@ -49,7 +49,7 @@ public class MainActivity extends AppCompatActivity
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
-        
+
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
