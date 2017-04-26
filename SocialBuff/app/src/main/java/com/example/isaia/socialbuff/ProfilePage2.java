a28a4b29f48a9ea0642c01e17a5075497cc73bd6
Isaiah <issi7915@colorado.edu>
2017-04-26 02:09:42 -0600

    Profile Updated with more functionality

--- SocialBuff/app/src/main/java/com/example/isaia/socialbuff/ProfilePage2.java
+++ SocialBuff/app/src/main/java/com/example/isaia/socialbuff/ProfilePage2.java
@@ -11,9 +11,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -24,7 +28,6 @@ public class ProfilePage2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_page2);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        ListView listView = (ListView) findViewById(R.id.list);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
         /**String[] values = new String[] {"\n" +
@@ -35,16 +38,14 @@ public class ProfilePage2 extends AppCompatActivity {
                 "        HOBBIES: Gymnastics, Hiking, etc.", "Date of birth", "Other"};*/
         String[] values = new String[]{"BoulderBandidio","AGE: 22",
                 "MAJOR: Electrical and Computer Engineering",
-                "YEAR: 17\'","HOBBIES: Gymnastics, Hiking, etc."};
-                ArrayList<String> valueslist = new ArrayList<String>();
+                "YEAR: 17' ","HOBBIES: Gymnastics, Hiking, etc."};
+                //ArrayList<String> valueslist = new ArrayList<String>();
 
-        valueslist.addAll (Arrays.asList(values));
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, valueslist);
-        adapter.add("Yay lists!");
-        listView.setAdapter(adapter);
-        adapter.add("Yay lists!");
+        ArrayAdapter <String> profile_info = new ArrayAdapter<String>(this, R.layout.my_profile_items, values);
+        ListView my_info = (ListView) findViewById(R.id.list);
+        my_info.setAdapter(profile_info);
 
+        registerClickCallback();
         /* I removed this button I think
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -67,4 +68,31 @@ public class ProfilePage2 extends AppCompatActivity {
             }
         }, intentFilter);
     }
+    private void registerClickCallback() {
+        ListView list = (ListView) findViewById(R.id.list);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> paret, View viewClicked, int position, long id) {
+                TextView textView = (TextView) viewClicked;
+                String message;
+
+                if(position == 0){
+                    message = "You clicked # 0, which is string: " + textView.getText().toString();
+                }
+                else if(position == 1){
+                    message = "You clicked # 1, which is string: " + textView.getText().toString();
+                }
+                else if(position == 2){
+                    message = "You clicked # 2, which is string: " + textView.getText().toString();
+                }
+                else{
+                    message = "You clicked # 3, which is string: " + textView.getText().toString();
+                }
+                Toast.makeText(ProfilePage2.this, message, Toast.LENGTH_LONG).show();
+
+            }
+        });
+    }
 }
+
+
